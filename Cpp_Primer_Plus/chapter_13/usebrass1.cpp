de84c75b5845df4d87dc5916b85748247a4dd8cf
DragonNimble <48077655+DragonNimble@users.noreply.github.com>
2022-11-12 09:12:27 +0800

    Add files via upload

---
+++ Cpp_Primer_Plus/chapter_13/usebrass1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "brass.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    Brass Piggy("Porcelot pigg", 381299, 4000.00);
+    BrassPlus Hoggy("Horatio Hogg", 382288, 3000.00);
+    Piggy.ViewAcct();
+    cout << endl;
+    cout << "Depositing $1000 into the Hogg Account:\n";
+    Hoggy.Deposit(1000.00);
+    cout << "New balance: $" << Hoggy.Balance() << endl;
+    cout << "Withdrawing $4200 from the pigg Account:\n";
+    Piggy.Withdraw(4200.00);
+    cout << "Pigg acount balance: $" << Piggy.Balance() << endl;
+    cout << "Withdrawing $4200 from the Hogg Account:\n";
+    Hoggy.Withdraw(4200.00);
+    Hoggy.ViewAcct();
+
+    return 0;
+}
